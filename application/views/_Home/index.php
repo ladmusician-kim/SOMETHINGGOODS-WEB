a8abefd29b2f28f39139b9b8644b8711bc6bf248
김동진 <ladmusician.kim@gmail.com>
2015-07-29 17:58:39 +0900

    메인화면 html(php) 구

--- application/views/_Home/index.php
+++ application/views/_Home/index.php
@@ -1,27 +1,58 @@
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="jumbotron">
     <div class="container text-center">
-        <img src="<?php echo base_url()?>static/img/pic1.jpg" />
+        <img class="sg-main-img-1" src="<?php echo base_url()?>static/img/pic1.jpg" />
+        <img class="sg-main-img-2-1" src="<?php echo base_url()?>static/img/pic1.jpg" />
+        <img class="sg-main-img-2-2" src="<?php echo base_url()?>static/img/pic1.jpg" />
+        <img class="sg-main-img-3" src="<?php echo base_url()?>static/img/pic1.jpg" />
     </div>
 </div>
 
 <div class="container">
     <!-- Example row of columns -->
+    <div class="sg-main-label">
+        SOMETHING GOODS 상품들
+    </div>
     <div class="row">
         <div class="col-md-4">
+            <img class="sg-main-img-item" src="<?php echo base_url()?>static/img/pic1.jpg" />
             <h2>Heading</h2>
             <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div>
         <div class="col-md-4">
+            <img class="sg-main-img-item" src="<?php echo base_url()?>static/img/pic1.jpg" />
             <h2>Heading</h2>
             <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div>
         <div class="col-md-4">
+            <img class="sg-main-img-item" src="<?php echo base_url()?>static/img/pic1.jpg" />
             <h2>Heading</h2>
             <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+    </div>
+    <hr>
+</div>
+
+<div class="container">
+    <!-- Example row of columns -->
+    <div class="sg-main-label">
+        다른 작가의 상품들
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <img class="sg-main-img-item" src="<?php echo base_url()?>static/img/pic1.jpg" />
+            <h2>Heading</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        </div>
+        <div class="col-md-4">
+            <img class="sg-main-img-item" src="<?php echo base_url()?>static/img/pic1.jpg" />
+            <h2>Heading</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        </div>
+        <div class="col-md-4">
+            <img class="sg-main-img-item" src="<?php echo base_url()?>static/img/pic1.jpg" />
+            <h2>Heading</h2>
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
         </div>
     </div>
     <hr>
--- application/views/_Partial/main_slider.php
+++ application/views/_Partial/main_slider.php
@@ -1,9 +1,9 @@
 <div class="sg-main-slider">
     <div class="container">
         <ul class="bxslider">
-            <li><img src="/SOMETHINGGOODS/static/img/pic1.jpg" /></li>
-            <li><img src="/SOMETHINGGOODS/static/img/pic2.jpg" /></li>
-            <li><img src="/SOMETHINGGOODS/static/img/pic3.jpg" /></li>
+            <li><img style="height: 500px" src="/SOMETHINGGOODS/static/img/pic1.jpg" /></li>
+            <li><img style="height: 500px" src="/SOMETHINGGOODS/static/img/pic2.jpg" /></li>
+            <li><img style="height: 500px" src="/SOMETHINGGOODS/static/img/pic3.jpg" /></li>
         </ul>
     </div>
 </div>
